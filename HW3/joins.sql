b623c7bb6d4eadfcfbc830e495ce8e6b1338748b
Kamronbek541 <128393982+Kamronbek541@users.noreply.github.com>
2025-09-28 16:45:31 +0200

    Update joins.sql

--- HW3/joins.sql
+++ HW3/joins.sql
@@ -7,5 +7,9 @@ order by user_id
 LIMIT 30
 
 
-
+-- list all attendee and the events they are registered for
+SELECT u.name AS attendee_name,e.title AS event_title
+FROM Register_to AS rt
+JOIN Users AS u ON rt.user_id = u.user_id
+JOIN Events AS e ON rt.event_id = e.event_id;
 
